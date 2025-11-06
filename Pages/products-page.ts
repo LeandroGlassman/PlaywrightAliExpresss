e7b41cb0d09f4cdb91dcae677135758bb3787362
Leandro Glassman <glassmanleandro@gmail.com>
2025-11-06 00:11:51 -0300

    Debugged locators issues, framework is fully functional, item 2 of page 2 is attached to the report

--- Pages/products-page.ts
+++ Pages/products-page.ts
@@ -7,16 +7,20 @@ export class ProductsPage {
 
   constructor(page: Page) {
     this.page = page;
-    this.productsCards = page.locator("#card-list > div");
-    this.secondPage = page.locator(".comet-pagination-item-2");
+    this.productsCards = page.locator(
+      "#card-list .search-item-card-wrapper-gallery",
+    );
+    this.secondPage = page.locator(".comet-pagination .comet-pagination-item-2");
   }
 
   async navigateToSecondPage() {
-    await this.secondPage.scrollIntoViewIfNeeded();
+    await this.page.evaluate(() => window.scrollTo(0, document.body.scrollHeight));
+    await this.secondPage.waitFor({ state: "visible", timeout: 10000 });
     await this.secondPage.click();
   }
 
   async getProductsCount() {
+    await this.productsCards.first().waitFor({ state: "visible" });
     return await this.productsCards.count();
   }
 
